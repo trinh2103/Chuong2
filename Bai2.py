883f2b4e0f28a5a30c771c73b83e82ec178f51be
trinh2103 <129131952+trinh2103@users.noreply.github.com>
2023-03-30 19:50:52 +0700

    Add files via upload

---
+++ Bai2.py
@@ -0,0 +1,4 @@
+GiaNiemYet=int(input("Nhap gia niem yet: "))
+Chietkhau=int(input("Nhap chiet khau: "))
+VAT=(GiaNiemYet+Chietkhau)*(0.01)
+print("Gia ban: "+ str(GiaNiemYet-Chietkhau+VAT))
---
+++ Bai3.py
@@ -0,0 +1,4 @@
+P=int(input("Tien dau tu ban dau: "))
+r=float(input("Lai suat moi thang: "))
+n=int(input("So thang gui: "))
+print("Tien lanh cuoi ky: "+str(P*(1+r*n)))
